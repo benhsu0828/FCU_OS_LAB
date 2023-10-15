5836d24ee3430b4599b5fe7a9f90e5041712e98e
benhsu0828 <benten.hsu@gmail.com>
2023-10-15 20:53:09 +0800

    HW2

--- HW2/D1053020_1.c
+++ HW2/D1053020_1.c
@@ -7,94 +7,79 @@
 
 struct student
 {
-	char *SID;
-	char *name;
-	char *class;
-	char *gender;				//0女1男
-	char *birthday;
-	struct list_head list;
+   char *SID;
+   char *name;
+   char *class;
+   char *gender;
+   char *birthday;
+   struct list_head list;
 };
 static LIST_HEAD(student_list);
 
-void insertData(student *ptr,char *SID, char *name, char *class, char *gender, char *birthday){
-	ptr = kmalloc(sizeof(*ptr), GFP_KERNEL);
-	if (ptr == NULL) {
-		return NULL;
-	}
-	student->SID = strdup(SID);
-	student->name = strdup(name);
-	student->class = strdup(class);
-	student->gender = strdup(gender);
-	student->birthday = strdup(birthday);
+void insertData(struct student *ptr, const char *SID, const char *name, const char *class, const char *gender, const char *birthday)
+{
+   // ptr = kmalloc(sizeof(*ptr), GFP_KERNEL);
+   //  if (ptr == NULL) {
+   //   return NULL;
+   //  }
+
+   ptr->SID = kmalloc(strlen(SID), GFP_KERNEL);
+   strcpy(ptr->SID, SID);
+
+   ptr->name = kmalloc(strlen(name), GFP_KERNEL);
+   strcpy(ptr->name, name);
+
+   ptr->class = kmalloc(strlen(class), GFP_KERNEL);
+   strcpy(ptr->class, class);
+
+   ptr->gender = kmalloc(strlen(gender), GFP_KERNEL);
+   strcpy(ptr->gender, gender);
+
+   ptr->birthday = kmalloc(strlen(birthday), GFP_KERNEL);
+   strcpy(ptr->birthday, birthday);
 }
 
 int simple_init(void)
 {
-	struct student *ptr;
-	struct student *person, *person2, *person3, *person4, *person5;
+   struct student *ptr;
+   struct student *person, *person2, *person3, *person4, *person5;
 
-	insertData(person, "D1053020", "PAO-HUA-HSU", "資訊三丁", "男", "92/08/28");
-	insertData(person2, "D1053021", "PAO-HUA-HSU1", "資訊三丁", "女", "92/08/29");
-	insertData(person3, "D1053022", "PAO-HUA-HSU2", "資訊三丁", "男", "92/08/30");
-	insertData(person4, "D1053023", "PAO-HUA-HSU3", "資訊三丁", "女", "92/08/31");
-	insertData(person5, "D1053024", "PAO-HUA-HSU4", "資訊三丁", "男", "92/09/01");
+   person = kmalloc(sizeof(*person), GFP_KERNEL);
+   person2 = kmalloc(sizeof(*person2), GFP_KERNEL);
+   person3 = kmalloc(sizeof(*person3), GFP_KERNEL);
+   person4 = kmalloc(sizeof(*person4), GFP_KERNEL);
+   person5 = kmalloc(sizeof(*person5), GFP_KERNEL);
 
-	// person = kmalloc(sizeof(*person), GFP_KERNEL);
-	// person->SID = 22;
-	// person->month = 7;
-	// person->year = 2018;
-	// person->name = kmalloc(sizeof("Jane"), GFP_KERNEL);
-	// strcpy(person->name, "Jane");
+   insertData(person, "D1053020", "PAO-HUA-HSU", "資訊三丁", "男", "92/08/28");
+   insertData(person2, "D1053021", "PAO-HUA-HSU1", "資訊三丁", "女", "92/08/29");
+   insertData(person3, "D1053022", "PAO-HUA-HSU2", "資訊三丁", "男", "92/08/30");
+   insertData(person4, "D1053023", "PAO-HUA-HSU3", "資訊三丁", "女", "92/08/31");
+   insertData(person5, "D1053024", "PAO-HUA-HSU4", "資訊三丁", "男", "92/09/01");
 
-	// person2 = kmalloc(sizeof(*person2), GFP_KERNEL);
-	// person2->day = 4;
-	// person2->month = 3;
-	// person2->year = 2018;
-	// person2->name = kmalloc(sizeof("Michael"), GFP_KERNEL);
-	// strcpy(person2->name, "Michael");
+   INIT_LIST_HEAD(&person->list);
+   list_add_tail(&person->list, &student_list);
+   list_add_tail(&person2->list, &student_list);
+   list_add_tail(&person3->list, &student_list);
+   list_add_tail(&person4->list, &student_list);
+   list_add_tail(&person5->list, &student_list);
 
-	// person3 = kmalloc(sizeof(*person3), GFP_KERNEL);
-	// person3->day = 4;
-	// person3->month = 3;
-	// person3->year = 2018;
-	// person3->name = kmalloc(sizeof("Lacey"), GFP_KERNEL);
-	// strcpy(person3->name, "Lacey");
-
-	// person4 = kmalloc(sizeof(*person2), GFP_KERNEL);
-	// person4->day = 4;
-	// person4->month = 3;
-	// person4->year = 2018;
-	// person4->name = kmalloc(sizeof("Michael"), GFP_KERNEL);
-	// strcpy(person4->name, "Michael");
-
-	// person5 = kmalloc(sizeof(*person3), GFP_KERNEL);
-	// person5->day = 4;
-	// person5->month = 3;
-	// person5->year = 2018;
-	// person5->name = kmalloc(sizeof("Lacey"), GFP_KERNEL);
-	// strcpy(person5->name, "Lacey");
-
-	list_add(&person->list, &student_list);
-	list_add(&person2->list, &student_list);
-	list_add(&person3->list, &student_list);
-	list_add(&person4->list, &student_list);
-	list_add(&person5->list, &student_list);
-	printk(KERN_INFO "loading moudle\n");
-	list_for_each_entry(ptr, &student_list, list){
-		printk(KERN_ALERT"SID: %s, Name: %s, Class: %s, Gender: %s, Birthday: %s\n",ptr->SID, ptr->name, ptr->class, ptr->gender, ptr->birthday);
-	}
-	return 0;
+   printk(KERN_INFO "loading moudle\n");
+   list_for_each_entry(ptr, &student_list, list)
+   {
+      printk(KERN_ALERT "SID: %s, Name: %s, Class: %s, Gender: %s, Birthday: %s\n", ptr->SID, ptr->name, ptr->class, ptr->gender, ptr->birthday);
+   }
+   return 0;
 }
 
 void simple_exit(void)
 {
-	struct student *ptr, *next;
-	list_for_each_entry_safe(ptr, next, &student_list, list)
-	{
-		list_del(&ptr->list);
-		kfree(ptr);
-	}
-	printk(KERN_INFO "Removing module\n");
+   struct student *ptr, *next;
+   list_for_each_entry_safe(ptr, next, &student_list, list)
+   {
+      list_del(&ptr->list);
+      kfree(ptr);
+   }
+   printk(KERN_INFO "Removing module\n");
 }
 
 module_init(simple_init);
--- HW2/D1053020_2.c
+++ HW2/D1053020_2.c
@@ -5,82 +5,90 @@
 #include <linux/list.h>
 #include <linux/slab.h>
 
-struct birthday
+struct student
 {
-	int day;
-	int month;
-	int year;
-	char *name;
-	struct list_head list;
+   char *SID;
+   char *name;
+   char *class;
+   char *gender;
+   char *birthday;
+   struct list_head list;
 };
-static LIST_HEAD(birthday_list);
+static LIST_HEAD(student_list);
+
+void insertData(struct student *ptr, const char *SID, const char *name, const char *class, const char *gender, const char *birthday)
+{
+   // ptr = kmalloc(sizeof(*ptr), GFP_KERNEL);
+   //  if (ptr == NULL) {
+   //   return NULL;
+   //  }
+
+   ptr->SID = kmalloc(strlen(SID), GFP_KERNEL);
+   strcpy(ptr->SID, SID);
+
+   ptr->name = kmalloc(strlen(name), GFP_KERNEL);
+   strcpy(ptr->name, name);
+
+   ptr->class = kmalloc(strlen(class), GFP_KERNEL);
+   strcpy(ptr->class, class);
+
+   ptr->gender = kmalloc(strlen(gender), GFP_KERNEL);
+   strcpy(ptr->gender, gender);
+
+   ptr->birthday = kmalloc(strlen(birthday), GFP_KERNEL);
+   strcpy(ptr->birthday, birthday);
+}
 
 int simple_init(void)
 {
-	struct birthday *ptr;
-	struct birthday *person, *person2, *person3, *person4, *person5;
+   struct student *ptr;
+   struct student *person, *person2, *person3, *person4, *person5;
+   struct student *firstp, *lastp;
 
-	person = kmalloc(sizeof(*person), GFP_KERNEL);
-	person->day = 22;
-	person->month = 7;
-	person->year = 2018;
-	person->name = kmalloc(sizeof("Jane"), GFP_KERNEL);
-	strcpy(person->name, "Jane");
+	firstp = kmalloc(sizeof(*firstp), GFP_KERNEL);
+	lastp = kmalloc(sizeof(*lastp), GFP_KERNEL);
+	insertData(firstp, "D1051111", "Leonardo", "資訊三丁", "男", "92/10/10");
+   	insertData(lastp, "D1051122", "ben", "資訊三丁", "女", "92/05/04");
+	INIT_LIST_HEAD(&firstp->list);
+	list_add(&firstp->list, &student_list);
+	list_add_tail(&lastp->list, &student_list);
 
-	person2 = kmalloc(sizeof(*person2), GFP_KERNEL);
-	person2->day = 4;
-	person2->month = 3;
-	person2->year = 2018;
-	person2->name = kmalloc(sizeof("Michael"), GFP_KERNEL);
-	strcpy(person2->name, "Michael");
+   person = kmalloc(sizeof(*person), GFP_KERNEL);
+   person2 = kmalloc(sizeof(*person2), GFP_KERNEL);
+   person3 = kmalloc(sizeof(*person3), GFP_KERNEL);
+   person4 = kmalloc(sizeof(*person4), GFP_KERNEL);
+   person5 = kmalloc(sizeof(*person5), GFP_KERNEL);
 
-	person3 = kmalloc(sizeof(*person3), GFP_KERNEL);
-	person3->day = 4;
-	person3->month = 3;
-	person3->year = 2018;
-	person3->name = kmalloc(sizeof("Lacey"), GFP_KERNEL);
-	strcpy(person3->name, "Lacey");
+   insertData(person, "D1053020", "PAO-HUA-HSU", "資訊三丁", "男", "92/08/28");
+   insertData(person2, "D1053021", "PAO-HUA-HSU1", "資訊三丁", "女", "92/08/29");
+   insertData(person3, "D1053022", "PAO-HUA-HSU2", "資訊三丁", "男", "92/08/30");
+   insertData(person4, "D1053023", "PAO-HUA-HSU3", "資訊三丁", "女", "92/08/31");
+   insertData(person5, "D1053024", "PAO-HUA-HSU4", "資訊三丁", "男", "92/09/01");
 
-	person4 = kmalloc(sizeof(*person2), GFP_KERNEL);
-	person4->day = 4;
-	person4->month = 3;
-	person4->year = 2018;
-	person4->name = kmalloc(sizeof("Michael"), GFP_KERNEL);
-	strcpy(person4->name, "Michael");
+   INIT_LIST_HEAD(&person->list);
+   list_add_tail(&person->list, &student_list);
+   list_add_tail(&person2->list, &student_list);
+   list_add_tail(&person3->list, &student_list);
+   list_add_tail(&person4->list, &student_list);
+   list_add_tail(&person5->list, &student_list);
 
-	person5 = kmalloc(sizeof(*person3), GFP_KERNEL);
-	person5->day = 4;
-	person5->month = 3;
-	person5->year = 2018;
-	person5->name = kmalloc(sizeof("Lacey"), GFP_KERNEL);
-	strcpy(person5->name, "Lacey");
-
-	list_add(&person->list, &birthday_list);
-	list_add(&person2->list, &birthday_list);
-	list_add(&person3->list, &birthday_list);
-	list_add(&person4->list, &birthday_list);
-	list_add(&person5->list, &birthday_list);
-	return 0;
+   printk(KERN_INFO "loading moudle\n");
+   list_for_each_entry(ptr, &student_list, list)
+   {
+      printk(KERN_ALERT "SID: %s, Name: %s, Class: %s, Gender: %s, Birthday: %s\n", ptr->SID, ptr->name, ptr->class, ptr->gender, ptr->birthday);
+   }
+   return 0;
 }
-
-void list_add(*ptr){
-
-}
-
-void list_add_tail(*ptr){
-
-}
-
 
 void simple_exit(void)
 {
-	struct birthday *ptr, *next;
-	list_for_each_entry_safe(ptr, next, &birthdat_list, list)
-	{
-		list_del(&ptr->list);
-		kfree(ptr);
-	}
-	printk(KERN_INFO "Removing module\n");
+   struct student *ptr, *next;
+   list_for_each_entry_safe(ptr, next, &student_list, list)
+   {
+      list_del(&ptr->list);
+      kfree(ptr);
+   }
+   printk(KERN_INFO "Removing module\n");
 }
 
 module_init(simple_init);
